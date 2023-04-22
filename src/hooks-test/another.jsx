8c4aea410e925020aeaedb67e802709d1aed5011
esrafilelahi <essinho1121@gmail.com>
2023-04-22 23:16:26 +0330

    changinggggggg

--- src/hooks-test/another.jsx
+++ src/hooks-test/another.jsx
@@ -1,48 +1,48 @@
-// import { useQuery } from "react-query";
-// import { fetchPosts } from "../api";
-// import { queryClient } from "../../../App";
+import { useQuery } from "react-query";
+import { fetchPosts } from "../api";
+import { queryClient } from "../../../App";
 
-// function prefetch(newPage) {
-//   queryClient.prefetchQuery({
-//     queryFn: fetchPosts(newPage),
-//     queryKey: [`posts_${newPage}`],
-//   });
-// }
+function prefetch(newPage) {
+  queryClient.prefetchQuery({
+    queryFn: fetchPosts(newPage),
+    queryKey: [`posts_${newPage}`],
+  });
+}
 
-// export const usePostsQuery = (page = 0) => {
-//   return { ...useQuery([`posts_${page}`], fetchPosts(page)), prefetch };
-// };
+export const usePostsQuery = (page = 0) => {
+  return { ...useQuery([`posts_${page}`], fetchPosts(page)), prefetch };
+};
 
-// // =======================================================================================
+// =======================================================================================
 
-// import { useMutation } from "react-query";
-// import { deletePost } from "../api";
+import { useMutation } from "react-query";
+import { deletePost } from "../api";
 
-// export const useDeletePost = () => {
-//   return useMutation({
-//     mutationFn: (id) => {
-//       return deletePost(id);
-//     },
-//   });
-// };
+export const useDeletePost = () => {
+  return useMutation({
+    mutationFn: (id) => {
+      return deletePost(id);
+    },
+  });
+};
 
-// // =======================================================================================
+// =======================================================================================
 
-// const [page, setPage] = useState(0);
-// const { isLoading, data, isError, error, prefetch } = usePostsQuery(page);
-// const { mutate: deletePost, isLoading: isDeleting } = useDeletePost();
+const [page, setPage] = useState(0);
+const { isLoading, data, isError, error, prefetch } = usePostsQuery(page);
+const { mutate: deletePost, isLoading: isDeleting } = useDeletePost();
 
-// useEffect(() => {
-//   prefetch(1);
-// }, [prefetch]);
+useEffect(() => {
+  prefetch(1);
+}, [prefetch]);
 
-// const handlePageChange = (newPage) => {
-//   setPage(newPage);
-//   prefetch(newPage + 1);
-// };
+const handlePageChange = (newPage) => {
+  setPage(newPage);
+  prefetch(newPage + 1);
+};
 
-// const handleDeletePost = (id) => () => {
-//   deletePost(id);
-// };
+const handleDeletePost = (id) => () => {
+  deletePost(id);
+};
 
-// // =======================================================================================
+// =======================================================================================
